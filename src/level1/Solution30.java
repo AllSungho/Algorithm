c251c57890d1292e2501d47cf45879467780d86c
AllSungho <nanumeran@gmail.com>
2025-07-10 17:25:00 +0900

    Level 1 Algorithm

--- src/level1/Solution30.java
+++ src/level1/Solution30.java
@@ -1,18 +1,15 @@
 package level1;
 
-import java.util.*;
-
-public class Solution30 {
-    public static void main(String[] args) {
-        String s = "abcdef";
+class Solution30 {
+    public String solution(String s) {
         String answer = "";
-        if(s.length() % 2 == 0) {
-            answer = ((Character) s.charAt(s.length()/2)).toString();
-            answer += ((Character) s.charAt(s.length()/2 +1)).toString();
+        if(s.length()%2==0) {
+            answer = ((Character) s.charAt(s.length()/2 - 1)).toString();
+            answer += ((Character) s.charAt(s.length()/2)).toString();
         }
         else {
             answer = ((Character) s.charAt(s.length()/2)).toString();
         }
-        System.out.println(answer);
+        return answer;
     }
 }
