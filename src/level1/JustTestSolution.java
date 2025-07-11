d4fff6cce068e243270339d277f7fc9ae74eac4a
AllSungho <nanumeran@gmail.com>
2025-07-11 11:59:56 +0900

    Level 2 Algorithm

--- src/level1/JustTestSolution.java
+++ src/level1/JustTestSolution.java
@@ -1,5 +1,6 @@
 package level1;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class JustTestSolution {
@@ -7,7 +8,13 @@ public class JustTestSolution {
         int[] numbers = new int[]{2,4,5,6,10};
 
         int[] answer = Arrays.stream(numbers).map(num -> num*2).toArray();
+        int[][] arr1 = {{1,2},{2,3}};
+        int[][] arr2 = {{3,4},{5,6}};
+
+        JustTestSolution a = new JustTestSolution();
+
 
         System.out.println(Arrays.toString(answer));
     }
+
 }
