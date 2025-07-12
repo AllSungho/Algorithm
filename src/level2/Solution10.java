a3a2cdf3066ca39815be1dd155ddf38bc963f689
AllSungho <nanumeran@gmail.com>
2025-07-12 16:25:55 +0900

    Level 2 Algorithm

--- src/level2/Solution10.java
+++ src/level2/Solution10.java
@@ -1,7 +1,20 @@
 package level2;
 
-public class Solution10 {
+import java.util.Arrays;
+import java.lang.StringBuilder;
+import java.lang.Integer;
+
+class Solution10 {
     public static void main(String[] args) {
-        
+        System.out.println(solution(5));
+    }
+    public static int solution(int n) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        while(n != 0){
+            stringBuilder.append(n % 3);
+            n /= 3;
+        }
+        return Integer.parseInt(stringBuilder.toString(), 3);
     }
 }
