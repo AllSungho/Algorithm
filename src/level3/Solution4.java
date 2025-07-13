7034b6f469b0999b10c046ba78745ad3ef062359
AllSungho <nanumeran@gmail.com>
2025-07-13 17:01:43 +0900

    Level 3 Algorithm

---
+++ src/level3/Solution4.java
@@ -0,0 +1,17 @@
+package level3;
+
+import java.util.Arrays;
+
+public class Solution4 {
+    public static void main(String[] args) {
+        int[][] sizes = {{60, 50}, {30, 70}, {60, 30}, {80, 40}};
+        solution(sizes);
+
+    }
+    public static int solution(int[][] sizes) {
+        int answer = 0;
+        
+        System.out.println();
+        return answer;
+    }
+}
