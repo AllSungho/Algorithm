f5a37b7072e95bc7a883c007791e6d6e75d904b0
AllSungho <nanumeran@gmail.com>
2025-07-15 20:40:21 +0900

    Level 3 Algorithm

---
+++ src/level3/Solution6.java
@@ -0,0 +1,21 @@
+package level3;
+
+import java.util.*;
+
+public class Solution6 {
+    public static void main(String[] args) {
+        Map<String, Integer> hashMap = new HashMap<>() {{
+            put("zero", 0);
+            put("one", 1);
+            put("two", 2);
+            put("three", 3);
+            put("four", 4);
+            put("five", 5);
+            put("six", 6);
+            put("seven", 7);
+            put("eight", 8);
+            put("nine", 9);
+        }};
+
+    }
+}
