1e9b21b7dee14e46f9b497900b29e3cc73bc7b65
AllSungho <nanumeran@gmail.com>
2025-07-15 21:54:22 +0900

    Level 3 Algorithm

--- src/level3/Solution6.java
+++ src/level3/Solution6.java
@@ -4,6 +4,8 @@ import java.util.*;
 
 public class Solution6 {
     public static void main(String[] args) {
+        String s = "one4seveneight";
+        StringBuilder answer = new StringBuilder();
         Map<String, Integer> hashMap = new HashMap<>() {{
             put("zero", 0);
             put("one", 1);
@@ -16,6 +18,21 @@ public class Solution6 {
             put("eight", 8);
             put("nine", 9);
         }};
-
+        StringBuilder str = new StringBuilder();
+        for(int i=0; i<s.length(); i++){
+            str.append(s.charAt(i));
+            if(Character.isDigit(s.charAt(i))) {
+                answer.append(s.charAt(i));
+                str.setLength(0);
+                continue;
+            }
+            for(String j : hashMap.keySet()){
+                if(str.toString().equals(j)){
+                    answer.append(hashMap.get(j));
+                    str.setLength(0);
+                    break;
+                }
+            }
+        }
     }
 }
